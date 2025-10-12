bd37021cf18c6c1b3aacebbf14db4edaf49b315d
way-zer <himc.wicp@gmail.com>
2025-10-12 17:14:11 +0800

    🎨(gradle) use dependencies.constraints

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     defineModule("bootStrap") {}
     defineModule("coreBukkit") {
         dependsModule("coreLibrary", parent!!)
-        api("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
+        api("dev.folia:folia-api")
     }
     defineModule("superItem") {
         dependsModule("coreBukkit")
